f0ef8159e1b57913830b9aa2fef88d48a65d8844
877318438@qq.com <chen_yy@outlook.com>
2015-08-26 13:59:02 +0800

    First commit

---
+++ 厦门中考录取自由查询/Module1.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("请输入报名号号：（可打开中考志愿填报工具.exe中的SQ Lite文件查询）")
+        Dim bmh As String = Console.ReadLine()
+        Console.WriteLine("请输入准考证号前6位：（根据考试学校决定）")
+        Dim zkz6 As String = Console.ReadLine()
+        For i = 1 To 30
+            For i2 = 1 To 50
+                Dim number As String
+                Dim data As String
+                Dim s As Boolean = False
+                If i < 10 Then
+                    number = "0" & i
+                Else
+                    number = i
+                End If
+                If i2 < 10 Then
+                    number &= "0" & i2
+                Else
+                    number &= i2
+                End If
+                Try
+                    data = http.HttpPost("http://www.xmzskszx.net/pub/lqcx.aspx", "ScriptManager1=ScriptManager1%7CBtn&__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUKMTUxMTIyOTQ5N2RkLtRJMl87ErmEQn8ufkyZ%2F5eCfZ%2FFOoAVW44RZcgSE6o%3D&__VIEWSTATEGENERATOR=D6AFB629&__EVENTVALIDATION=%2FwEWBAKc7fjjAwLDgoO0AQLBgpu%2FAgKyoqqWD8MCaPYdf9%2BluHxgfGAxjMhvr7usgQvwPFx7eLx%2BApMS&TxtBMH=" & bmh & "&TxtZKZ=" & zkz6 & number & "&__ASYNCPOST=true&Btn=%20%20%E7%A1%AE%20%E5%AE%9A%20%20")
+                Catch ex As Exception
+                    Console.WriteLine("已查询准考证号：" & zkz6 & number & "失败")
+                    s = True
+                End Try
+                If s Then
+                Else
+                    If data.Contains("无录取信息。") Then
+                        Console.WriteLine("已查询准考证号：" & zkz6 & number & "无录取信息。")
+                    Else
+                        MsgBox(data)
+                    End If
+                End If
+            Next
+        Next
+        Console.WriteLine("已遍历，无结果")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ 厦门中考录取自由查询/http.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text
+
+Public Class http
+    Shared Function HttpPost(URL As String, PostData As String) As String
+        Dim request As HttpWebRequest = WebRequest.Create(URL)
+        request.Method = "POST"
+        Dim byteArray As Byte() = Encoding.UTF8.GetBytes(PostData)
+        request.ContentType = "application/x-www-form-urlencoded"
+        request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+        request.ContentLength = byteArray.Length
+        Dim dataStream As Stream = request.GetRequestStream()
+        dataStream.Write(byteArray, 0, byteArray.Length)
+        dataStream.Close()
+        Dim response As WebResponse = request.GetResponse()
+        If (CType(response, HttpWebResponse).StatusCode = HttpStatusCode.OK) Then
+            dataStream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+            reader.Close()
+            dataStream.Close()
+            response.Close()
+            Return responseFromServer
+        Else
+            Return ""
+        End If
+    End Function
+    Shared Function HttpGet(URL As String) As String
+        Dim request As WebRequest = WebRequest.Create(URL)
+        request.Credentials = CredentialCache.DefaultCredentials
+        Dim response As WebResponse = request.GetResponse()
+        If CType(response, HttpWebResponse).StatusCode = HttpStatusCode.OK Then
+            Dim dataStream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(dataStream)
+            Dim responseFromServer As String = reader.ReadToEnd()
+            reader.Close()
+            response.Close()
+            Return responseFromServer
+        Else
+            Return ""
+        End If
+    End Function
+End Class
